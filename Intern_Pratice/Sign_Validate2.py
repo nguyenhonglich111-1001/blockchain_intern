7e12ec36293055c9c6daa5bc5264e5819a4ed290
unknown <lichnguyenhong.it@gmail.com>
2022-05-05 16:17:29 +0700

    Trying to buy and sell

--- Intern_Pratice/Sign_Validate2.py
+++
@@ -1,7 +0,0 @@
-import rsa
-
-(pubkey, privkey) = rsa.newkeys(512)
-message = 'Go left at the blue tree'.encode()
-signature = rsa.sign(message, privkey, 'SHA-1')
-
-print(rsa.verify(message, signature, pubkey))
--- test.py
+++ test.py
@@ -1,13 +1,3 @@
-import math
-import secrets
+a = [[1,2],[3,4]]
 
-def generateTarget():
-    # generate 32byte -> 256 bit hash using secret
-    target = secrets.token_hex(32)
-    target = (int(str(target).strip(),16))
-
-    return target
-
-target = generateTarget()
-
-print(math.log10(target))
+print(3,4 in a)
